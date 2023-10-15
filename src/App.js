24e038adcc28bbd8da91af027c86473038253548
Vin R <vramroop6@gmail.com>
2023-10-15 18:31:11 -0400

    Jury rig method of website

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe
+        title="Robotics Website"
+        src="https://greasedlightningrobo.wixsite.com/robotics"
+        width="100%"
+        height="800px"
+        frameBorder="0"
+      />
+    </div>
+  );
+}
+
+export default App;
